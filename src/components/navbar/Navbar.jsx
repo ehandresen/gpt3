271630cfae200edfa0b5c80ac1711b6173d1aee2
ehandresen <eirik.horgen.andresen@gmail.com>
2024-05-18 16:52:46 +0200

    added assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,19 @@
 import './navbar.css';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+
+// BEM -> Block Element Modifier (CSS naming convention)
+// Syntax: [block]__[element]--[modifier]
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
